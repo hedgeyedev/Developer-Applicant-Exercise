15d2eb2ccac505c8c54283b1d5fa2413362fce24
Jason Rogers <jacaetevha@gmail.com>
2012-03-22 20:42:40 -0400

    forgot to add a few things

--- simple_public_timeline/app/text_converter.rb
+++ simple_public_timeline/app/text_converter.rb
@@ -1,6 +1,7 @@
 require 'uri'
 
 class TextConverter
+  BASE_URL              = 'https://twitter.com'
   HASH_TAGS_PATTERN     = /(#([\w]+))([\W]|$)/u
   MENTIONS_PATTERN      = /(@([\w]+))([\W]|$)/u
   SUPPORTED_URL_SCHEMES = %w{http https ftp sftp ssh scp rcp git git+ssh}
@@ -12,13 +13,13 @@ class TextConverter
   def self.convert_hashtags text
     text.gsub(HASH_TAGS_PATTERN) do
       # a.tweet-url.hashtag{:href => "/search?q=%23thetimeis", :rel => "nofollow", :title => "#thetimeis"}
-      "<a class='tweet-url hashtag' href='https://twitter.com/search?q=%23#{$2}' rel='nofollow' title='##{$2}'>##{$2}</a> "
+      "<a class='tweet-url hashtag' href='#{BASE_URL}/search?q=%23#{$2}' rel='nofollow' title='##{$2}'>##{$2}</a> "
     end
   end
 
   def self.convert_user_mentions text
     text.gsub(MENTIONS_PATTERN) do
-      "<a class='tweet-url username' href='https://twitter.com/#{$2}' rel='nofollow'>@#{$2}</a> "
+      "<a class='tweet-url username' href='#{BASE_URL}/#{$2}' rel='nofollow'>@#{$2}</a> "
     end
   end
   
