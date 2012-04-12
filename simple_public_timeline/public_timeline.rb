d773442db951c1a904435f4d58d2fd62e327ceec
David Angga Prasetya <carsmetic.oc@gmail.com>
2012-04-12 20:59:35 +0700

    adding index

--- simple_public_timeline/public_timeline.rb
+++ simple_public_timeline/public_timeline.rb
@@ -1,6 +1,17 @@
+require "sinatra"
 class PublicTimeline < Sinatra::Base
+  helpers do
+    def extract_source source
+      unless (source.to_s =~ /\&gt\;(.+?)\&lt/).nil?
+        string = $1
+      end
+      string
+    end
+  end
+
   get "/" do
-    
+    @results = Twitter.search("popular tweets", :rpp => 20, :result_type => "recent")
+    erb :index
   end
   
   get "via_js" do
