74e7d160f77d56ae0ab2bce73760747d2930495a
Ren Provey <ren.provey@gmail.com>
2012-06-03 11:40:02 -0400

    Added #altcode_from_req_id to isolate alcode creation logic, ease testing, and encourage reuse.

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,7 +1,7 @@
 # 1. Long method, looks like #template is building the altcode
 #    as well as swapping it in to the template.
 #
-# 2. Better naming all around:
+# 2. Better naming all around (uncommunicative names):
 #    - Template seems like a very generic and non-descript name.
 #      The generic-ness of it could lead to conflicts later, while
 #      the module's intended purpose isn't apparent by its name.
@@ -13,11 +13,18 @@
 # 3. Without more background, I'd lean towards having the
 #    altcode being the responsibility of the model to provide.
 #
-# 4. Again, without context, I'd say the module's template method would
+# 4. Altcode creation should be handled in it's own method to provide
+#    for reuse and ease of testing.
+#
+# 5. Again, without context, I'd say the module's template method would
 #    be best suited in the model it is being included on.  It seems
 #    to be very dependant on the data in that class.
 
 module Template
+  def altcode_from_req_id(req_id)
+    req_id[0..4] + "-" + req_id[5..7]
+  end
+
   def template(source_template, req_id)
     template = source_template
 
@@ -34,7 +41,7 @@ module Template
     template_split_end = template_split_begin + 9
     template_part_one = template[0..(template_split_begin-1)]
     template_part_two = template[template_split_end..template.length]
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two # looks like, appending part two is not covered in testing
+    #altcode = code[0..4] + "-" + code[5..7]
+    template_part_one + altcode_from_req_id(req_id) + template_part_two # looks like, appending part two is not covered in testing
   end
 end
--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -3,6 +3,12 @@ require_relative 'template'
 describe Template do
   include Template
 
+  describe "#altcode_from_req_id" do
+    it "should convert a req_id to an altcode" do
+      altcode_from_req_id("5678901234").should == '56789-012'
+    end
+  end
+
   describe "#template" do
     it "should substitute %CODE% and %ALTCODE% in the template" do
       template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
