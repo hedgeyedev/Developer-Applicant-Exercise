79c9610a376af42906fc9177c78782d266efdc80
Ren Provey <ren.provey@gmail.com>
2012-06-03 11:28:23 -0400

    Code cleanup, intitial thoughts.

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,40 @@
+# 1. Long method, looks like #template is building the altcode
+#    as well as swapping it in to the template.
+#
+# 2. Better naming all around:
+#    - Template seems like a very generic and non-descript name.
+#      The generic-ness of it could lead to conflicts later, while
+#      the module's intended purpose isn't apparent by its name.
+#    - The module is named Template, has a template method, and a
+#      argument named template, too many templates?
+#    - req_id is more descriptive name than code, using it avoids a
+#      second variable
+#
+# 3. Without more background, I'd lean towards having the
+#    altcode being the responsibility of the model to provide.
+#
+# 4. Again, without context, I'd say the module's template method would
+#    be best suited in the model it is being included on.  It seems
+#    to be very dependant on the data in that class.
+
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    template = source_template
 
     # Substitute for %CODE%
     template_split_begin = template.index("%CODE%")
     template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
+    template_part_one = template[0..(template_split_begin-1)]
+    template_part_two = template[template_split_end..template.length]
     code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = template_part_one + code + template_part_two
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
     template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
+    template_part_one = template[0..(template_split_begin-1)]
+    template_part_two = template[template_split_end..template.length]
     altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    template_part_one + altcode + template_part_two # looks like, appending part two is not covered in testing
   end
 end
