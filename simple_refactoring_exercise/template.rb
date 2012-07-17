6b88df355eba9af1544fff1bdb03ede5b54febc9
Andrew Purdy <andrewmp1@gmail.com>
2012-07-17 00:45:40 -0700

    Factor our markup substitution from both methods.

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -4,31 +4,24 @@ module Template
 
     # Substitute for %CODE%
     def substitute_for_code(template, req_id)
-      template_split_begin = template.index("%CODE%")
-      template_split_end = template_split_begin + 6
-      template_part_one =
-        template[0..(template_split_begin-1)]
-      template_part_two =
-        template[template_split_end..template.length]
-      code = req_id
-      template =
-        template_part_one + code + template_part_two
+      substitute_markup(template, "%CODE%", req_id)
     end
 
 
     # Substitute for %ALTCODE%
     def substitute_for_altcode(template, req_id)
-      code = req_id
-      template_split_begin = template.index("%ALTCODE%")
-      template_split_end = template_split_begin + 9
-      template_part_one =
-        template[0..(template_split_begin-1)]
-      template_part_two =
-        template[template_split_end..template.length]
-      altcode = code[0..4] + "-" + code[5..7]
-      template_part_one + altcode + template_part_two
+      substitute_markup(template, "%ALTCODE%", "#{req_id[0..4]}-#{req_id[5..7]}")
     end
 
-    substitute_for_code(substitute_for_altcode(template, req_id), req_id)
+    def substitute_markup(template, markup, value)
+      template_split_begin = template.index(markup)
+      template_split_end = template_split_begin + markup.length
+      template_part_one = template[0..(template_split_begin-1)]
+      template_part_two = template[template_split_end..template.length]
+      code = value
+      template_part_one + code + template_part_two
+    end
+
+    substitute_for_altcode(substitute_for_code(template, req_id), req_id)
   end
 end
