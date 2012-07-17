95bd4a6d5ae5293d539196b19e98402c68df4387
Andrew Purdy <andrewmp1@gmail.com>
2012-07-17 00:59:16 -0700

    Pull out logic and put it into a class

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,16 +1,22 @@
 module Template
   def template(source_template, req_id)
-    template = source_template
+     TemplateConverter.new(source_template, req_id).convert
+  end
+
+  class TemplateConverter
+    def initialize(source_template, req_id)
+       @template, @req_id = source_template, req_id
+    end
 
     # Substitute for %CODE%
-    def substitute_for_code(template, req_id)
-      substitute_markup(template, "%CODE%", req_id)
+    def substitute_for_code(template = @template)
+      substitute_markup(template, "%CODE%", @req_id)
     end
 
 
     # Substitute for %ALTCODE%
-    def substitute_for_altcode(template, req_id)
-      substitute_markup(template, "%ALTCODE%", alt_code(req_id))
+    def substitute_for_altcode(template = @template)
+      substitute_markup(template, "%ALTCODE%", alt_code(@req_id))
     end
 
     def substitute_markup(template, markup, value)
@@ -21,6 +27,9 @@ module Template
       "#{code[0..4]}-#{code[5..7]}"
     end
 
-    substitute_for_altcode(substitute_for_code(template, req_id), req_id)
+    def convert
+      substitute_for_altcode(substitute_for_code(@template))
+    end
+
   end
 end
