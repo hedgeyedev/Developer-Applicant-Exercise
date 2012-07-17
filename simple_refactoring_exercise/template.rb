2500cef3e15f1a96a2d98d5dd59825f50245ab35
Andrew Purdy <andrewmp1@gmail.com>
2012-07-17 00:32:49 -0700

    create methods inside template for substituting code & altcode in order to refactor each

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -3,24 +3,32 @@ module Template
     template = source_template
 
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      template[0..(template_split_begin-1)]
-    template_part_two =
-      template[template_split_end..template.length]
-    code = req_id
-    template =
-      template_part_one + code + template_part_two
+    def substitute_for_code(template, req_id)
+      template_split_begin = template.index("%CODE%")
+      template_split_end = template_split_begin + 6
+      template_part_one =
+        template[0..(template_split_begin-1)]
+      template_part_two =
+        template[template_split_end..template.length]
+      code = req_id
+      template =
+        template_part_one + code + template_part_two
+    end
+
 
     # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      template[0..(template_split_begin-1)]
-    template_part_two =
-      template[template_split_end..template.length]
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    def substitute_for_altcode(template, req_id)
+      code = req_id
+      template_split_begin = template.index("%ALTCODE%")
+      template_split_end = template_split_begin + 9
+      template_part_one =
+        template[0..(template_split_begin-1)]
+      template_part_two =
+        template[template_split_end..template.length]
+      altcode = code[0..4] + "-" + code[5..7]
+      template_part_one + altcode + template_part_two
+    end
+
+    substitute_for_code(substitute_for_altcode(template, req_id), req_id)
   end
 end
