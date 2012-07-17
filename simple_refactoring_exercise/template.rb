63bb6781b2e2690d3d9c8e6b1b019958892d41f8
Andrew Purdy <andrewmp1@gmail.com>
2012-07-17 00:25:26 -0700

    Remove calls to String.new() on strings

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,25 +1,25 @@
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    template = source_template
 
     # Substitute for %CODE%
     template_split_begin = template.index("%CODE%")
     template_split_end = template_split_begin + 6
     template_part_one =
-      String.new(template[0..(template_split_begin-1)])
+      template[0..(template_split_begin-1)]
     template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
+      template[template_split_end..template.length]
+    code = req_id
     template =
-      String.new(template_part_one + code + template_part_two)
+      template_part_one + code + template_part_two
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
     template_split_end = template_split_begin + 9
     template_part_one =
-      String.new(template[0..(template_split_begin-1)])
+      template[0..(template_split_begin-1)]
     template_part_two =
-      String.new(template[template_split_end..template.length])
+      template[template_split_end..template.length]
     altcode = code[0..4] + "-" + code[5..7]
     template_part_one + altcode + template_part_two
   end
