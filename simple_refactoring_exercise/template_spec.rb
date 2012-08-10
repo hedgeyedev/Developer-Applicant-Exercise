25df7d9d71e2270ef820602922ee8b663484700b
Yuriy Goldshtrakh <ygoldshtrakh@gmail.com>
2012-08-09 17:49:41 -0700

    fixed spec for missing space

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -8,7 +8,7 @@ describe Template do
   end
   
   it "should substitute multiple occurances of %CODE% and %ALTCODE% in the template" do
-    template('Code is %CODE%; alt code is %ALTCODE%, and dup %CODE% with one more %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012, and dup 5678901234with one more 56789-012'
+    template('Code is %CODE%; alt code is %ALTCODE%, and dup %CODE% with one more %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012, and dup 5678901234 with one more 56789-012'
   end
 
   it "should substitute %ALTCODE%, without %CODE% in the tempalte" do
