96ed12a3d18da617ef97ed7a78b295bdfafd871c
Yuriy Goldshtrakh <ygoldshtrakh@gmail.com>
2012-08-09 17:56:08 -0700

    all specs pass

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,6 +1,10 @@
 module Template
   def template(source_template, req_id)
-    altcode = req_id[0..4] + "-" + req_id[5..7]
+    begin
+      altcode = req_id[0..4] + "-" + req_id[5..7]
+    rescue
+      altcode = req_id
+    end
     source_template.gsub(/\%CODE\%/,req_id).gsub(/\%ALTCODE\%/,altcode)
   end
 end
