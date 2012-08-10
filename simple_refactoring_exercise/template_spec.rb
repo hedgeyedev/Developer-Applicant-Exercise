544106b1afae34bcc316036dc98db70a3b64ff4d
Yuriy Goldshtrakh <ygoldshtrakh@gmail.com>
2012-08-09 17:38:17 -0700

    added one more example, currently failing

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -6,5 +6,8 @@ describe Template do
   it "should substitute %CODE% and %ALTCODE% in the template" do
     template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
   end
-  
+  it "should substitute multiple occurances of %CODE% and %ALTCODE% in the template" do
+    template('Code is %CODE%; alt code is %ALTCODE%, and dup %CODE% with one more %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012, and dup 5678901234with one more 56789-012'
+
+  end
 end
