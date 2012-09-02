2d1b47dd62c0882eb345f25d4ebed5abb272bbdd
Leo Medovyy <lmedovyy@gmail.com>
2012-09-02 10:16:27 -0700

    added additonal test cases, modified tempalte to ensrue that all tests pass

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,7 +1,6 @@
 module Template
     def template(source_template, req_id)
         altcode = "#{req_id[0..4]}-#{req_id[5..7]}"
-        source_template.gsub!("%CODE%",req_id)
-        source_template.gsub!("%ALTCODE%",altcode)
+        source_template.gsub(/\%CODE%/,req_id).gsub(/\%ALTCODE%/,altcode)
     end
 end
--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -7,4 +7,16 @@ describe Template do
     template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
   end
   
+  it "should substitute %CODE% in the template without %ALTCODE%" do
+    template('Code is %CODE%', '5678901234').should == 'Code is 5678901234'
+  end
+
+  it "should substitute %ALTCODE% in the template without %CODE%" do
+    template('Code is %ALTCODE%', '5678901234').should == 'Code is 56789-012'
+  end
+
+  it "should substitute %CODE% and %ALTCODE% in the template twice" do
+    template('Code is %CODE%; alt code is %ALTCODE%; Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012; Code is 5678901234; alt code is 56789-012'
+  end
+
 end
