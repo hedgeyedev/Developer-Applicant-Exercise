b8e14dc8f4afa04ed2dbbb0df4e9e278b8f3a54c
Leo Medovyy <lmedovyy@gmail.com>
2012-09-02 10:28:17 -0700

    added rails app and rvm file

---
+++ simple_public_timeline/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplePublicTimeline::Application.config.secret_token = 'c8ca644f78a9b0eb18f737221bfe2781ad8e3f754f50394a47d90bf61c0473160289c53e702b81d43f62440ebd0c579630b705e3c66622845d9e78dd7155702f'
