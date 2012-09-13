31b156d86e3e6e8732cc537ffc30451a7e3e92f1
Scott Moore <oberonix@gmail.com>
2012-09-13 00:23:57 -0400

    Fixed tabbing

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -5,7 +5,7 @@ module Template
 	raise ArgumentError, "req_id must at least 8 characters long" unless req_id.length >= 8
 	raise ArgumentError, "source_template must be a String" unless source_template.kind_of? String
 	
-    #setup altcode
+	#setup altcode
 	altcode = req_id[0..4] + "-" + req_id[5..7]
 	
 	#use regex to find the variables, replace with enum
