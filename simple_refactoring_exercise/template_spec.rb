8f281111358194bc3a2b34bb23ea6ecb6952b7b1
offmango <info@etk.me>
2013-02-03 11:57:57 -0500

    did simple refactoring exercise

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -3,8 +3,16 @@ require_relative 'template'
 describe Template do
   include Template
 
-  it "should substitute %CODE% and %ALTCODE% in the template" do
-    template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
+  context "#template" do
+	it "should substitute %CODE% and %ALTCODE% in the template" do
+    	template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
+  	end
   end
-  
+
+  context "#alter_req_id" do
+  	it "should convert a req_id of format 5678901234 into format 56789-012" do
+  		alter_req_id("1234567890").should == "12345-678"
+  	end
+  end
+
 end
