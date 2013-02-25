b5331e1f4571b0c7c8c32a8f6d80b6d8baf77d46
VB <VB@IMAC-BOND.local>
2013-02-25 10:42:32 -0500

    Refactor

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -5,22 +5,32 @@ module Template
     # Substitute for %CODE%
     template_split_begin = template.index("%CODE%")
     template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
     code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = gen_template(code)
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    template_split_end = template_split_begin + 9  
+    
+    # altcode unnecessary, changing value of code instead - altcode = code[0..4] + "-" + code[5..7]
+    code = String.new(code[0..4] + "-" + code[5..7])
+    
+    template = gen_template(code)
+    
+    # Was invalid code
+    #template_part_one + altcode + template_part_two
+    
   end
+  
+  #Move duplicate string operations into separate method
+ 
+  def gen_template(code)
+      template_part_one =
+      String.new(template[0..(template_split_begin-1)])
+      template_part_two =
+      String.new(template[template_split_end..template.length])
+      template =
+      String.new(template_part_one + code + template_part_two)
+   end
+
 end
