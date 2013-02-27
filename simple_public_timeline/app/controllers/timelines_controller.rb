5489d62962ccce5f9bfda75ef44f8825f3b5d36c
Renaldo Pierre-Louis <pirelouisd87@gmail.com>
2013-02-27 11:00:37 -0500

    specs and refactor

--- simple_public_timeline/app/controllers/timelines_controller.rb
+++ simple_public_timeline/app/controllers/timelines_controller.rb
@@ -1,6 +1,8 @@
 class TimelinesController < ApplicationController
+  include TimelinesHelper
   require 'tweetstream'
   # GET /timelines
+  # Get /timelines.json
   def index
     @tweets = get_tweets
     respond_to do |format|
@@ -9,26 +11,10 @@ class TimelinesController < ApplicationController
     end
   end
 
+  # Get /via_js
   def js
     respond_to do |format|
       format.html
     end
-  end
-
-  def get_tweets
-    TweetStream.configure do |config|
-      config.consumer_key       = 'iXW2fg9yeqJ1MEFehkGFKw'
-      config.consumer_secret    = 'AmTHg0N9zcCLEBn4Kg32N3YGllR5fwg3aeoTH4WSTA'
-      config.oauth_token        = '162784302-EqvQbAfhLFpdQ34m6SDFTygwisMrJBR5UfUjKhNs'
-      config.oauth_token_secret = 'NDZQvteFFDxMhqSBWUrUUVXVHCWpOmIA7Nr3Fo8nog'
-      config.auth_method        = :oauth
-    end
-
-    tweets = []
-    TweetStream::Client.new.sample do |status, client|
-      tweets << status
-      client.stop if tweets.size >= 20
-    end
-    tweets
   end
 end
--- simple_public_timeline/db/migrate/20130224172911_create_timelines.rb
+++
@@ -1,8 +0,0 @@
-class CreateTimelines < ActiveRecord::Migration
-  def change
-    create_table :timelines do |t|
-
-      t.timestamps
-    end
-  end
-end
