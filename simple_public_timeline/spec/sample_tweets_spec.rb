8e4c6287495b44de58ba1d5a11481a0c9ae71ba0
James Lavin <james@jameslavin.com>
2013-03-08 19:23:31 -0500

    fix serialization problem (confusing Tweets object with Tweets' tweets array)

--- simple_public_timeline/spec/sample_tweets_spec.rb
+++ simple_public_timeline/spec/sample_tweets_spec.rb
@@ -1,7 +1,10 @@
-require_relative '../sample_tweets_app'
+require 'tweetstream'
+require 'twitter_filter'
 require 'rspec'
 require 'rack/test'
 require 'yaml'
+require 'json'
+require_relative '../sample_tweets_app'
 
 set :environment, :test
 
@@ -9,14 +12,23 @@ describe 'the SampleTweets app' do
   include Rack::Test::Methods
 
   let(:twenty_tweets_file) { File.expand_path("../../fixtures/tweets/twenty_tweets.yml", __FILE__) }
-  let(:twenty_tweets) { YAML.load(File.read(twenty_tweets_file)) }
+  let(:twenty_tweets) { Tweets.deserialize(twenty_tweets_file) }
 
   def app
     Sinatra::Application
   end
 
   before do
-    GetTweets.any_instance.stub(:sample).and_return(twenty_tweets)
+    GetTweets.any_instance.stub(:sample).with(20).and_return(twenty_tweets)
+  end
+
+  describe '#get_sample' do
+  
+    it "works" do
+      get_sample.class.name.should == 'Tweets'
+      get_sample.tweets.length.should == 20
+    end
+
   end
 
   describe '/' do
@@ -36,11 +48,9 @@ describe 'the SampleTweets app' do
   
     it "returns the tweets as JSON" do
     
-      get '/tweets.json'
+      header "HTTP_ACCEPT", "application/json"
+      get '/tweets.json', {'Content-Type' => 'application/json'}
       last_response.should be_ok
-      last_response.body.should include "Que pecha de reir hoy en el cambio de clase"
-      last_response.body.should include "bixychick"
-      last_response.body.should include "1st show 11 yrs ago, good times, I'd do it all again."
 
     end
   
