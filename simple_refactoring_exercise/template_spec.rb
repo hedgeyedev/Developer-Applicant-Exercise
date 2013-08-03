0579c012b13e1863f0216687907dbc294dc99752
Ryan Kohler <ryan.c.kohler@gmail.com>
2013-08-02 17:31:10 -0700

    refactored template

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -7,4 +7,12 @@ describe Template do
     template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
   end
   
+  it "should not alter either original string" do
+    tmpl = 'Code is %CODE%; alt code is %ALTCODE%'
+    code = '5678901234'
+    template(tmpl,code)
+    tmpl.should == 'Code is %CODE%; alt code is %ALTCODE%'
+    code.should == '5678901234'
+  end
+  
 end
