e67d89fc7c4f845ecd534d5b41ce615015634834
Forrest Chang <fchang@hedgeye.com>
2014-01-10 16:33:38 -0800

    [finishes #63654286] update rspec to expect syntax

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -4,7 +4,7 @@ describe Template do
   include Template
 
   it "should substitute %CODE% and %ALTCODE% in the template" do
-    template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
+    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
   end
-  
+
 end
