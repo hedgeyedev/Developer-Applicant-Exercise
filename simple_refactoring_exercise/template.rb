934cf1118110fa7fb819fe6ec09a5879341949ba
Don Humphreys <dhumphreys88@gmail.com>
2015-07-23 02:38:41 -0400

    Refactored template method

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,17 @@
+# I fixed the following code smells to simplify the implementation
+# - long method
+# - duplicate code (refactored using ruby string methods)
+# - complexity (split up responsibility with private method to format alt code)
+# - excessive casting (used to_s and known return types)
+
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    source_template.to_s.gsub('%CODE%', req_id).gsub('%ALTCODE%', altcode(req_id))
+  end
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+  private
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+  def altcode(code)
+    code[0..4] + "-" + code[5..7]
   end
 end
--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -3,8 +3,17 @@ require_relative 'template'
 describe Template do
   include Template
 
-  it "should substitute %CODE% and %ALTCODE% in the template" do
-    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
-  end
+  describe '.template' do
+    it 'substitutes code' do
+      expect(template('Code is %CODE%', '5678901234')).to eq('Code is 5678901234')
+    end
 
+    it 'substitutes alt code' do
+      expect(template('alt code is %ALTCODE%', '5678901234')).to eq('alt code is 56789-012')
+    end
+
+    it "substitutes both codes" do
+      expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq('Code is 5678901234; alt code is 56789-012')
+    end
+  end
 end
