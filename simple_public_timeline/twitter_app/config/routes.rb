e823e24c87c0bd53a3134377b649437e6a855f52
ankit <ankit.chachada@gmail.com>
2016-04-11 23:09:49 -0700

    Changed routes for Hedgeye development intern exercise

--- simple_public_timeline/twitter_app/config/routes.rb
+++ simple_public_timeline/twitter_app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/javascript_way'
+  get  'via_js', to: 'home#javascript_way'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
