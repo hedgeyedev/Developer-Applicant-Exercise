620ab4cec8e5eeeaf991b24147143854014fc1f3
ankit <ankit.chachada@gmail.com>
2016-04-11 22:18:15 -0700

    Changes

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,23 @@
+# I have refactored the by putting the duplicated code in one method 
+# and call that method wherever required
 module Template
   def template(source_template, req_id)
     template = String.new(source_template)
-
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
+    sub = sub(template,"%CODE%")
     code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
-
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
+    template = String.new(sub[0] + code + sub[1])
+    new_sub = sub(template,"%ALTCODE%")
     altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    new_sub[0] + altcode + new_sub[1]
   end
+
+  def sub(template,code)
+    template_split_begin = template.index(code)
+    template_split_end = template_split_begin + code.length
+    template_part_one = String.new(template[0..(template_split_begin-1)])
+    template_part_two = String.new(template[template_split_end..template.length])
+    return template_part_one,template_part_two
+  end
+  
 end
+
