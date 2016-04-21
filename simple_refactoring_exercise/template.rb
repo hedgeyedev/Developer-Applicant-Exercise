f55db54eead9442b3f6a716a4b03d639dc4da2f3
ankit <ankit.chachada@gmail.com>
2016-04-21 00:09:07 -0700

    modified template.rb

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,21 +1,20 @@
-# I have refactored the by putting the duplicated code in one method 
-# and call that method wherever required
+# I have refactored the code by putting the duplicated code in one method 
+# and call that method wherever required.
+#Also I just modified the code and removed String.new as it was not needed
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
-    sub = sub(template,"%CODE%")
-    code = String.new(req_id)
-    template = String.new(sub[0] + code + sub[1])
+    sub = sub(source_template,"%CODE%")
+    template = sub[0] + req_id + sub[1]
     new_sub = sub(template,"%ALTCODE%")
-    altcode = code[0..4] + "-" + code[5..7]
+    altcode = req_id[0..4] + "-" + req_id[5..7]
     new_sub[0] + altcode + new_sub[1]
   end
 
   def sub(template,code)
     template_split_begin = template.index(code)
     template_split_end = template_split_begin + code.length
-    template_part_one = String.new(template[0..(template_split_begin-1)])
-    template_part_two = String.new(template[template_split_end..template.length])
+    template_part_one = template[0..(template_split_begin-1)]
+    template_part_two = template[template_split_end..template.length]
     return template_part_one,template_part_two
   end
   
