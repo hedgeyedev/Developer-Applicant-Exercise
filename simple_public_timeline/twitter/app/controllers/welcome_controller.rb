85fc70445b1cb41ce6838d8af8e860de28bfe2b4
Lizzie <lizzie.sorin@hotmail.fr>
2018-11-15 16:46:57 -0800

    server display tweets correctly but no link in text

--- simple_public_timeline/twitter/app/controllers/welcome_controller.rb
+++ simple_public_timeline/twitter/app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+  	@tweets = ApiTwitter.public_tweets
   end
+
+  def viajs
+  end
+  
 end
--- simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
+++ simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
@@ -1,30 +1,54 @@
 # require 'omniauth'
 require 'oauth'
 require 'json'
-require "/.code"
+require 'dotenv'
+
+
+# class ApiTwitter
+# 	CONSUMER_KEY = ENV['API_KEY']
+# 	CONSUMER_SECRET = ENV['API_SECRET']
+# 	OA_TOKEN = ENV['OAUTH_TOKEN']
+# 	OA_SECRET = ENV['OAUTH_SECRET']
+# 	BASE_URI = 'https://api.twitter.com/1.1/'
+
+
+# 	def initialize
+# 		 consumer = OAuth::Consumer.new("CONSUMER_KEY", "CONSUMER_SECRET", { :site => "https://api.twitter.com", :scheme => :header })
+# 		 token_hash = { :oauth_token => OA_TOKEN, :oauth_token_secret => OA_SECRET }
+# 		 @access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+# 	end
+
+# 	def query(method_name, path, options=nil)
+# 		JSON.parse(@access_token.request(method_name, "#{BASE_URI}#{path}", options).body)
+# 	end
+
+# 	def home_line 
+# 		response = query(:get, 'statuses/home_timeline.json')	
+# 	end
+
+# end
+
+# a = ApiTwitter.new
+# a.home_line
 
 class ApiTwitter
-	CONSUMER_KEY = API_KEY
-	CONSUMER_SECRET = API_SECRET
-	OA_TOKEN = OAUTH_TOKEN
-	OA_SECRET = OAUTH_SECRET
+	CONSUMER_KEY = ENV['API_KEY']
+	CONSUMER_SECRET = ENV['API_SECRET']
+	OA_TOKEN = ENV['OAUTH_TOKEN']
+	OA_SECRET = ENV['OAUTH_SECRET']
 	BASE_URI = 'https://api.twitter.com/1.1/'
 
-
-	def initialize
-		 consumer = OAuth::Consumer.new("CONSUMER_KEY", "CONSUMER_SECRET", { :site => "https://api.twitter.com", :scheme => :header })
-		 token_hash = { :oauth_token => OA_TOKEN, :oauth_token_secret => OA_SECRET }
-		 @access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+	def self.public_tweets
+		client.user_timeline("HEDGEYE", count:1, exclude_replies: true)
 	end
 
-	def query(method_name, path, options=nil)
-		JSON.parse(@access_token.request(method_name, "#{BASE_URI}#{path}", options).body)
+	def self.client
+		@client ||= Twitter::REST::Client.new do |config|
+			config.consumer_key = CONSUMER_KEY
+			config.consumer_secret = CONSUMER_SECRET
+		end
 	end
 
-	def home_line 
-		response = query(:get, 'statuses/home_timeline.json')	
-	end
+
 end
 
-a = ApiTwitter.new
-a.home_line
