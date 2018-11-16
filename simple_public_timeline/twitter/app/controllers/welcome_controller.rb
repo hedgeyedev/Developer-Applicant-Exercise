5332515d9f8dc62aee9cf0dc50fbb8f215fadaf2
Lizzie <lizzie.sorin@hotmail.fr>
2018-11-15 22:42:05 -0800

    receive tweets from TweetStream

--- simple_public_timeline/twitter/app/controllers/welcome_controller.rb
+++ simple_public_timeline/twitter/app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def index
-  	@tweets = ApiTwitter.public_tweets
+  	# @tweets = ApiTwitter.public_tweets
+  	@tweets = TweetsStream.public_tweets
   end
 
   def viajs
   end
-  
+
 end
--- simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
+++ simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
@@ -11,44 +11,70 @@ require 'dotenv'
 # 	OA_SECRET = ENV['OAUTH_SECRET']
 # 	BASE_URI = 'https://api.twitter.com/1.1/'
 
-
-# 	def initialize
-# 		 consumer = OAuth::Consumer.new("CONSUMER_KEY", "CONSUMER_SECRET", { :site => "https://api.twitter.com", :scheme => :header })
-# 		 token_hash = { :oauth_token => OA_TOKEN, :oauth_token_secret => OA_SECRET }
-# 		 @access_token = OAuth::AccessToken.from_hash(consumer, token_hash )
+# 	def self.public_tweets
+# 		client.user_timeline("BBCNews", count:1, exclude_replies: true)
 # 	end
 
-# 	def query(method_name, path, options=nil)
-# 		JSON.parse(@access_token.request(method_name, "#{BASE_URI}#{path}", options).body)
+# 	def self.client
+# 		@client ||= Twitter::REST::Client.new do |config|
+# 			config.consumer_key = CONSUMER_KEY
+# 			config.consumer_secret = CONSUMER_SECRET
+# 		end
 # 	end
 
-# 	def home_line 
-# 		response = query(:get, 'statuses/home_timeline.json')	
-# 	end
 
 # end
 
 # a = ApiTwitter.new
 # a.home_line
 
-class ApiTwitter
-	CONSUMER_KEY = ENV['API_KEY']
-	CONSUMER_SECRET = ENV['API_SECRET']
-	OA_TOKEN = ENV['OAUTH_TOKEN']
-	OA_SECRET = ENV['OAUTH_SECRET']
-	BASE_URI = 'https://api.twitter.com/1.1/'
+require 'tweetstream'
 
-	def self.public_tweets
-		client.user_timeline("BBCNews", count:1, exclude_replies: true)
-	end
+class TweetsStream
+  # config.consumer_key       = ENV['API_KEY']
+  # config.consumer_secret    = ENV['API_SECRET']
+  # config.oauth_token        = ENV['OAUTH_TOKEN']
+  # config.oauth_token_secret = ENV['OAUTH_SECRET']
+  # config.auth_method        = :oauth
+
+
+# This will pull a sample of all tweets based on
+# your Twitter account's Streaming API role.
+
+TweetStream.configure do |config|
+  config.consumer_key       = ENV['API_KEY']
+  config.consumer_secret    = ENV['API_SECRET']
+  config.oauth_token        = ENV['OAUTH_TOKEN']
+  config.oauth_token_secret = ENV['OAUTH_SECRET']
+  config.auth_method        = :oauth
+end
 
 	def self.client
-		@client ||= Twitter::REST::Client.new do |config|
-			config.consumer_key = CONSUMER_KEY
-			config.consumer_secret = CONSUMER_SECRET
+		@client ||= TweetStream::Client.new 
+	end
+
+
+
+	def self.public_tweets
+		@statuses = []
+		client.sample do |status|
+		  # The status object is a special Hash with
+		  # method access to its keys.
+		  @statuses << status
+		  client.stop if @statuses.size >= 10
 		end
+		@statuses
+	
+	end
+
+	def self.statuses
+		return @statuses
 	end
 
 
 end
+
+# @client = TweetsStream.new
+
+
 
