8b42e565f1b9080c936b6be3b34ad7f983dc5268
Lizzie <lizzie.sorin@hotmail.fr>
2018-11-17 06:05:21 -0800

    create commit action

--- simple_public_timeline/twitter/app/controllers/welcome_controller.rb
+++ simple_public_timeline/twitter/app/controllers/welcome_controller.rb
@@ -1,12 +1,19 @@
 class WelcomeController < ApplicationController
+before_action :find_tweets
+
   def index
-  	@tweets = TweetsStream.public_tweets
   end
 
   def tweetsapi
-  		@tweets = TweetsStream.public_tweets
-		#creating an API end point and using to render via_js
-		render json: {status: "SUCCESS", message:'Loaded tweets', data:@tweets}, status: :ok
+	render json: {status: "SUCCESS", message:'Loaded tweets', data:@tweets}, status: :ok
+  end
+
+  def bonus
+  end
+
+  private
+  def find_tweets
+  	@tweets = TweetsStream.public_tweets
   end
 
 end
--- simple_public_timeline/twitter/config/routes.rb
+++ simple_public_timeline/twitter/config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :tweets
-  get 'welcome/index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 root to: 'welcome#index'
 get '/via_js', to: 'welcome#viajs'
 get '/tweetsapi', to: 'welcome#tweetsapi'
+get '/bonus', to: 'welcome#bonus'
 
 end
