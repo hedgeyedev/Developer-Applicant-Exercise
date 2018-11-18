43caf25f5cf2fe298d054766ed499679fc191cc3
Lizzie <lizzie.sorin@hotmail.fr>
2018-11-18 11:13:30 -0800

    cleanup

--- simple_public_timeline/twitter/app/models/tweet.rb
+++
@@ -1,3 +0,0 @@
-class Tweet < ApplicationRecord
-	belongs_to :user
-end
--- simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
+++ simple_public_timeline/twitter/config/initializers/application_controller_renderer.rb
@@ -43,32 +43,6 @@ end
 
 
 
-# class ApiTwitter
-# 	CONSUMER_KEY = ENV['API_KEY']
-# 	CONSUMER_SECRET = ENV['API_SECRET']
-# 	OA_TOKEN = ENV['OAUTH_TOKEN']
-# 	OA_SECRET = ENV['OAUTH_SECRET']
-# 	BASE_URI = 'https://api.twitter.com/1.1/'
-
-# 	def initialize
-# 		consumer = OAuth::Consumer.new(CONSUMER_KEY, CONSUMER_SECRET, { site: 'https://api.twitter.com', scheme: :header})
-# 		token_hash = { oauth_token: OA_TOKEN, oauth_token_secret: OA_SECRET }
-# 		@access_token = OAuth::AccessToken.from_hash(consumer, token_hash)
-# 	end
-
-# 	def query(method_name, path, options=nil)
-# 		JSON.parse(@access_token.request(method_name, "#{BASE_URI}#{path}", options).body)
-# 	end
-
-# 	def home_line
-# 		response = query(:get, 'statuses/home_timeline.json')
-# 	end
-
-
-# end
-
-#  a = ApiTwitter.new
-#  a.home_line
 
 
 
