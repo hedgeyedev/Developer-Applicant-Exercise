d9e53e249a3ea2c85ad23c861127d7ac9a85961f
Lizzie <lizzie.sorin@hotmail.fr>
2018-11-18 07:59:44 -0800

    extended API testing

--- simple_public_timeline/twitter/spec/requests/controllers_spec.rb
+++ simple_public_timeline/twitter/spec/requests/controllers_spec.rb
@@ -4,7 +4,16 @@ describe "Welcome", type: :request do
 	describe "tweetsapi" do
 
 		let!(:tweet) do
-			Twitter::Tweet.new(id: 1)
+			Twitter::Tweet.new(
+				created_at: "Sun Nov 18 14:46:08 +0000 2018",
+				id: 1064167880558731300,
+				text: "RT @ValaAfshar: You are not your job. You are so much more. This elementary school surprised their ‘superhero’ custodian with an appreciat…",
+			 	source: "<a href='http://twitter.com/download/android' rel='nofollow'>Twitter for Android</a>",
+				user: {
+					screen_name: "OregonFembot",
+					profile_image_url_https: "https://pbs.twimg.com/profile_images/873256968269332480/YbwkX-Bu_normal.jpg"
+				}
+			)
 		end 
 
 		before do
@@ -22,8 +31,34 @@ describe "Welcome", type: :request do
 		end
 		it "should return tweets" do
 			get "/tweetsapi"
-			parsed_body = JSON.parse(response.body)
-			expect(parsed_body['tweets']).to eql([{"id" => 1}])
+			parsed_body = JSON.parse(response.body)	
+			expect(parsed_body['tweets'][0]["id"]).to eql(1064167880558731300)
 		end
+		it "should return tweets" do
+			get "/tweetsapi"
+			parsed_body = JSON.parse(response.body)	
+			expect(parsed_body['tweets'][0]["created_at"]).to eql("Sun Nov 18 14:46:08 +0000 2018")
+		end
+		it "should return tweets" do
+			get "/tweetsapi"
+			parsed_body = JSON.parse(response.body)	
+			expect(parsed_body['tweets'][0]["text"]).to eql("RT @ValaAfshar: You are not your job. You are so much more. This elementary school surprised their ‘superhero’ custodian with an appreciat…")
+		end
+		it "should return sources" do
+			get "/tweetsapi"
+			parsed_body = JSON.parse(response.body)
+			expect(parsed_body['tweets'][0]["source"]).to eql("<a href='http://twitter.com/download/android' rel='nofollow'>Twitter for Android</a>")
+		end
+		it "should return sources" do
+			get "/tweetsapi"
+			parsed_body = JSON.parse(response.body)
+			expect(parsed_body['tweets'][0]["user"]["screen_name"]).to eql("OregonFembot")
+		end
+		it "should return sources" do
+			get "/tweetsapi"
+			parsed_body = JSON.parse(response.body)
+			expect(parsed_body['tweets'][0]["user"]["profile_image_url_https"]).to eql("https://pbs.twimg.com/profile_images/873256968269332480/YbwkX-Bu_normal.jpg")
+		end
+
 	end
 end
