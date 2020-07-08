574fa63231fd17451a6c4f6fcf1013300a4bfe6a
edhernandez04 <mchh.004@gmail.com>
2020-07-07 23:56:33 -0400

    completed

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,34 +1,18 @@
 require 'pry'
 
 module Template
-  # source_template = 5678901234
-  # req_id = 20
   # 'Code is 5678901234; alt code is 56789-012'
 
   def template(source_template, req_id)
     template = String.new(source_template)
 
     # Substitute for %CODE%
-    code_template = []
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
     code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = String.new(String.new(template[0..(template.index("%CODE%")-1)]) + code + String.new(template[template.index("%CODE%")+6..template.length]))
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
+    # Substitute for %ALTCODE% 
     altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    String.new(template[0..(template.index("%ALTCODE%")1)]) + altcode + String.new(template_split_begin + 9..template.length])
   end
 
 end
