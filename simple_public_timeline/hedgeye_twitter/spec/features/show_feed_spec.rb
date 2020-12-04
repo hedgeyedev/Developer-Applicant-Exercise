2ceb852fdc4312be275933c11fcfc5c4814c5cd0
Collin Jilbert <cjilbert504@gmail.com>
2020-12-04 16:13:34 -0600

    Add spec for sub heading and complete feature

--- simple_public_timeline/hedgeye_twitter/spec/features/show_feed_spec.rb
+++ simple_public_timeline/hedgeye_twitter/spec/features/show_feed_spec.rb
@@ -1,8 +1,13 @@
 require "./spec/spec_helper.rb"
 
 RSpec.describe "Viewing the server side feed" do
-	it "has a main headline" do
+	it "has a main heading" do
 		visit "/"
 		expect(page).to have_text("Recent Public Tweets")
+	end
+
+	it "has a sub-heading" do
+		visit "/"
+		expect(page).to have_text("What everyone on Twitter is talking about")
 	end
 end
