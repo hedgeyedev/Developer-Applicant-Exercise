8e5bfcdfeb48bdd1aa67e8f1dab2d454523d5c74
Michael McGuire <michael@mcguire.ch>
2021-02-26 15:14:00 -0500

    refactored template and added more tests

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -4,7 +4,14 @@ describe Template do
   include Template
 
   it "should substitute %CODE% and %ALTCODE% in the template" do
-    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
+    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234', 5, 2)).to eq 'Code is 5678901234; alt code is 56789-012'
   end
 
+  it "should substitute %CODE% and %ALTCODE% in the template when index is 3 and drop is 4" do
+    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234', 3, 4)).to eq 'Code is 5678901234; alt code is 567-890'
+  end
+
+  it "should substitute %CODE% and %ALTCODE% in the template even when code is frozen" do
+    expect(template('Code is %CODE%; alt code is %ALTCODE%'.freeze, '5678901234', 3, 4)).to eq 'Code is 5678901234; alt code is 567-890'
+  end
 end
