ab3297c9b36962226766dd1da24c6d34ad4b418c
Abe Millett <abemillett@protonmail.com>
2021-03-14 17:55:32 -0400

    initial commit of refactoring, new method is functional

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,10 +1,23 @@
 module Template
-  def template(source_template, req_id)
+
+  @@tag_definitions = Hash[
+    "CODE"    => lambda { |code| code },
+    "ALTCODE" => lambda { |code| code.to_s[0..4] + "-" + code.to_s[5..7] }
+  ]
+
+  def template_old(source_template, req_id)
+    # non-descriptive parameter name "req_id"?
     template = String.new(source_template)
+
+    # 1. DRY this out ("Duplicated Code")
+    # 2. is there a better way to manipulate strings? This seems verbose/clunky ("Shotgun Surgery")
+    # 3. is the template method the best place to get req_id? maybe it is. That would depend on who
+    #    or what is using this module and how? Can/should the interface be changed? (if yes, this
+    #    could be a case of "Long Parameter List")
 
     # Substitute for %CODE%
     template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
+    template_split_end = template_split_begin + 6 # magic number
     template_part_one =
       String.new(template[0..(template_split_begin-1)])
     template_part_two =
@@ -15,12 +28,28 @@ module Template
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
+    template_split_end = template_split_begin + 9 # magic number
     template_part_one =
       String.new(template[0..(template_split_begin-1)])
     template_part_two =
       String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    altcode = code[0..4] + "-" + code[5..7] # magic numbers
+    template_part_one + altcode + template_part_two # ambiguous? what should this return?
   end
+
+  def template(source_template, req_id)
+
+    template = String.new(source_template)
+    # should req_id also be enforced to a string? or should that be handled in the tag definition?
+
+    @@tag_definitions.each do |tag, replace|
+      template.gsub!("%#{tag}%", replace.call(req_id))
+    end
+
+    return template
+
+  end
+
+  module_function :template
+  module_function :template_old
 end
--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -7,4 +7,8 @@ describe Template do
     expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
   end
 
+  it "the old and the new methods should yield the same result" do
+    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')
+  end
+
 end
