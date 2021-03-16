21001ee1d0ee2b40f0649364d93efd993bd1816a
srolandmarshall <samuel.r.marshall@gmail.com>
2021-03-15 22:23:28 -0400

    added notes to refactor

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -9,5 +9,9 @@ module Template
     # Substitute for %ALTCODE%
     altcode = code[0..4] + "-" + code[5..7]
     template.gsub!("%ALTCODE%", altcode)
+
+    # I replaced the index-finding code and string splicing with Ruby String's `gsub!` method, which finds a substring and replaces it within a string. The ! means that the method is descructive, and thus prevents the need from extra declarative statements. Further, there was variable reuse which causes confusion and can introduce bugs easily.
+
+    # The reason this is better than the provided code is that the previously provided code created a bunch of new string objects that take up additional memory every iteration. The above is much less memory intensive, less redundant, and much more human readable. 
   end
 end
