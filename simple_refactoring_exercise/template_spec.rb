a55f1d10e3be5806ec7c75c6a3b9306f441b3887
John Faithorn <toymakerlabs@gmail.com>
2021-03-21 20:10:59 +0000

    Add additional test cases and revise
    Revise to use gsub and add additional test cases to check for the
    order of the templates, and if the template replacement contains
    invalid characters.

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -7,4 +7,15 @@ describe Template do
     expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
   end
 
+  it "should substitute %ALTCODE% and %CODE% in the template" do
+    expect(template('Alt code is %ALTCODE% and code is %CODE%', '5678901234')).to eq 'Alt code is 56789-012 and code is 5678901234'
+  end
+
+  it "should substitute more than once instance of %ALTCODE% and %CODE%" do
+    expect(template('Alt codes are %ALTCODE%, %ALTCODE% and two codes are %CODE%, %CODE%', '5678901234')).to eq 'Alt codes are 56789-012, 56789-012 and two codes are 5678901234, 5678901234'
+  end
+
+  it "should error if contains %" do
+    expect {template('Code is %CODE%', '5678901234%')}.to raise_error StandardError
+  end
 end
