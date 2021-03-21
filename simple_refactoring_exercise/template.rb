576b3b3e243088d0d8302fbaf3e652d6e9d67696
John Faithorn <toymakerlabs@gmail.com>
2021-03-21 19:36:29 +0000

    Add observations to template
    Add initial refactoring observations to template.rb to aid in adding
    new test cases.

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,4 +1,23 @@
+=begin
+Things I noticed right away:
+1. %CODE% and %ALTCODE% look redundant.
+  It looks like altcode should replace code,
+  but substring the first eight digits and insert a dash before the last two.
+
+2. What happens if a template string includes a % character?
+  Do we restrict to numbers?
+  
+3. The template_split_begin + index doesn't look very flexible.
+  What if we need to change our template placeholders
+
+4. What if we changed the order of %CODE% and %ALTCODE%?
+
+5. What if we have more than one instance of %CODE% and %ALTCODE%?
+=end
 module Template
+  CODE = "%CODE%"
+  ALTCODE = "%ALTCODE%"
+
   def template(source_template, req_id)
     template = String.new(source_template)
 
