1ae69c520b39f0178d010158c50083190f413236
seanrobenalt <srob0722@gmail.com>
2021-08-29 14:50:37 -0400

    ruby implementation

---
+++ simple_public_timeline/app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+
+require 'dotenv'
+Dotenv.load
+
+get '/' do
+  require_relative 'services/twitter/api'
+
+  client = Twitter::Api.client
+
+  tweets = []
+  
+  client.sample do |object|
+    if object.is_a?(Twitter::Tweet)
+      tweets.push({
+        source: object.source,
+        url: object.url,
+        user: {
+          name: object.user.name,
+          photo: object.user.profile_image_url_https
+        },
+        text: object.text,
+        created_at: object.created_at
+      }) 
+    end
+
+    break if tweets.count == 20
+  end
+
+  @tweets = tweets
+  erb :index
+end
---
+++ simple_public_timeline/services/twitter/api.rb
@@ -0,0 +1,14 @@
+require 'twitter'
+
+module Twitter
+  class Api
+    def self.client
+      Twitter::Streaming::Client.new do |config|
+        config.consumer_key        = ENV['TWITTER_CONSUMER_KEY']
+        config.consumer_secret     = ENV['TWITTER_CONSUMER_SECRET']
+        config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
+        config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+      end
+    end
+  end
+end
