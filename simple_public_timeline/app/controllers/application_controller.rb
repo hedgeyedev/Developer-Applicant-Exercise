b773cbb322651411db449bb56a911db30f1e9810
wayne <wayne.noonan@gmail.com>
2021-09-07 07:51:37 -0500

    removing creds, didn't mean to commit them

--- simple_public_timeline/app/controllers/application_controller.rb
+++ simple_public_timeline/app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ require 'sinatra/base'
 
 class ApplicationController < Sinatra::Base
   TweetStream.configure do |config|
-    config.consumer_key = '5AzUk4AgxgM9bNQQ93mWbQS1e'
-    config.consumer_secret = '1RxMU2hb2csrr18ZrZzp2DcMIWRtyOKSM7y5CU3dTIrCU2qv3l'
-    config.oauth_token = '1435020993089712135-sDLmFzH4BfSwyyMKsdDjuLJcFC6kv4'
-    config.oauth_token_secret = 'iXZ2KzxM1V4oOgYz2J2HpNjRQuSmhWmsE659R4uyGVl0t'
+    config.consumer_key = ''
+    config.consumer_secret = ''
+    config.oauth_token = ''
+    config.oauth_token_secret = ''
     config.auth_method = :oauth
   end
 
