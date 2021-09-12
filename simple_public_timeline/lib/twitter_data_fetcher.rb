2ce1274d8320a5a22834af367be51e416909fed5
[Replace with Your Name] <[Replace with Your Morton Email]>
2021-09-12 15:04:03 -0500

    Implement get tweets method in twitter data fetcher module

--- simple_public_timeline/lib/twitter_data_fetcher.rb
+++ simple_public_timeline/lib/twitter_data_fetcher.rb
@@ -1,3 +1,20 @@
-def get_tweets()
-    return ['1', '2']
+require 'twitter'
+
+module TwitterDataFetcher
+    $twitter_streaming_client = Twitter::Streaming::Client.new do |config|
+        config.consumer_key        = ''
+        config.consumer_secret     = ''
+        config.access_token        = ''
+        config.access_token_secret = ''
+    end
+
+    def self.get_tweets(num_tweets = 20)
+        tweets = []
+
+        $twitter_streaming_client.sample do |tweet|
+            return tweets if tweets.length() == num_tweets
+
+            tweets << tweet.user.profile_image_url_https if tweet.is_a?(Twitter::Tweet)
+        end
+    end
 end
