92ead341cb265ea2019cebf5e3fe60e93dc23259
[Replace with Your Name] <[Replace with Your Morton Email]>
2021-09-12 22:03:55 -0500

    Add logic to check env var creds before running app

--- simple_public_timeline/app.rb
+++ simple_public_timeline/app.rb
@@ -2,6 +2,23 @@ require 'sinatra'
 
 require_relative './lib/twitter_data_fetcher.rb'
 
+# Raise error if Twitter API credentials env variables are not set
+if ( ENV['TWITTER_CONSUMER_KEY']        == nil ||
+     ENV['TWITTER_CONSUMER_SECRET']     == nil ||
+     ENV['TWITTER_ACCESS_TOKEN']        == nil ||
+     ENV['TWITTER_ACCESS_TOKEN_SECRET'] == nil
+)
+    raise %q(
+        Credentials to authentication to Twitter's API not configured.
+
+        Set and export the following env variables with the appropriate values before re-running the app:
+            - TWITTER_CONSUMER_KEY
+            - TWITTER_CONSUMER_SECRET
+            - TWITTER_ACCESS_TOKEN
+            - TWITTER_ACCESS_TOKEN_SECRET
+    )
+end
+
 get '/' do
     tweets = TwitterDataFetcher::get_tweets()
     erb :index, :locals => { :tweets => tweets }
