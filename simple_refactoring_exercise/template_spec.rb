51a7d131309ca50214b6484eeebe148480b4aa27
Justin Myers <justin@colortechnology.com>
2022-01-31 14:54:50 -0800

    refactor the spec a little bit for readability.

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -4,7 +4,10 @@ describe Template do
   include Template
 
   it "should substitute %CODE% and %ALTCODE% in the template" do
-    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
+    template = 'Code is %CODE%; alt code is %ALTCODE%'
+    code = '5678901234'
+    result = 'Code is 5678901234; alt code is 56789-012'
+    expect(template(template, code)).to eq result
   end
 
 end
