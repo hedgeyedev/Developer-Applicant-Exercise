7f7ca7f99378e1165e1156dbdea8585af12a6c7b
Justin Myers <justin@colortechnology.com>
2022-01-31 15:47:30 -0800

    Refactor the template. Introduce a code formatter to handle responsibility for knowing how to present the code. Replace the complicated and duplicated insertion logic with gsub.

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,23 @@
 module Template
   def template(source_template, req_id)
     template = String.new(source_template)
+    code_formatter = CodeFormatter.new(req_id)
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = template.gsub("%CODE%", code_formatter.plain_code)
+    template = template.gsub("%ALTCODE%", code_formatter.altcode)
+  end
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+  class CodeFormatter
+    def initialize(code)
+      @raw_code = String.new(code)
+    end
+
+    def plain_code
+      @code ||= @raw_code
+    end
+
+    def altcode
+      @altcode ||= @raw_code[0..4] + "-" + @raw_code[5..7]
+    end
   end
 end
