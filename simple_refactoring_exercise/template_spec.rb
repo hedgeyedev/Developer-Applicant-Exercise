a8efa96afe17c429f9d7fae51969c8c7c3139ccb
Justin Myers <justin@colortechnology.com>
2022-01-31 18:05:40 -0800

    Spec out the CodeFormatter for fun.

--- simple_refactoring_exercise/template_spec.rb
+++ simple_refactoring_exercise/template_spec.rb
@@ -10,5 +10,18 @@ describe Template do
     result = 'Code is 5678901234; alt code is 56789-012'
     expect(template(template, code)).to eq result
   end
+end
 
+describe Template::CodeFormatter do
+  before(:all) do
+    @code_formatter = Template::CodeFormatter.new('CATSARENICEANIMALS')
+  end
+
+  it "should provide the code in plain format" do
+    expect(@code_formatter.plain_code).to eq 'CATSARENICEANIMALS'
+  end
+
+  it "should provide the code in an alternative format" do 
+    expect(@code_formatter.altcode).to eq 'CATSA-REN'
+  end
 end
