e7563307efcd3e060ded8bea4b19186d33378b97
Integerous <iwantobe@snu.ac.kr>
2023-11-14 01:06:15 -0800

    Refactor template

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,33 @@
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    formatter = Formatter.new(req_id)
+    Replacer.new(source_template, formatter).replace
+  end
+end
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+class Replacer
+  def initialize(template, formatter)
+    @template = String.new(template)
+    @formatter = formatter
+  end
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+  def replace
+    template = @template.gsub("%CODE%", @formatter.normal_code)
+    template.gsub("%ALTCODE%", @formatter.alt_code)
+  end
+end
+
+class Formatter
+  def initialize(code)
+    raise "Code must be 10 characters long" if code.length != 10
+    @code = code
+  end
+
+  def normal_code
+    @code
+  end
+
+  def alt_code
+    @code[0..4] + "-" + @code[5..7]
   end
 end
