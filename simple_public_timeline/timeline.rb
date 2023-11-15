82546bb20cdc4ee22fcd292403df1e62048852b9
Integerous <iwantobe@snu.ac.kr>
2023-11-14 16:17:19 -0800

    Call jsonplaceholder API instead of Twitter API

--- simple_public_timeline/timeline.rb
+++ simple_public_timeline/timeline.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'net/http'
+require 'uri'
 
 get '/' do
+  uri = URI("https://jsonplaceholder.typicode.com/photos")
+  response = Net::HTTP.get(uri)
+  @results = JSON.parse(response).take(20)
+
   erb :index, layout: :main
 end
