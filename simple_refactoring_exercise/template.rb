99b1d1eb36e4138eec66ca005e264ca9b0819df9
integerous <iwantobe@snu.ac.kr>
2023-11-16 23:13:35 -0800

    Add comments about what I refactored and why

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,3 +1,10 @@
+# 1. Extracts template replacement logic into separate Formatter and Replacer classes.
+#   - By creating dedicated classes, the code becomes more modular, adhering to the Single Responsibility Principle.
+#   - Each class has a clear purpose: Formatter for formatting the code strings, and Replacer for handling the template replacement.
+# 2. Utilizes gsub, a more concise and idiomatic way of replacing strings in Ruby.
+#   - This tackles the code smell of Duplication and Complexity in the original code.
+# 3. Encapsulates the state and behavior related to formatting and replacing in its respective classes.
+#   - This Enhances maintainability and clarity. Each class is responsible for its own data and behavior.
 module Template
   def template(source_template, req_id)
     formatter = Formatter.new(req_id)
