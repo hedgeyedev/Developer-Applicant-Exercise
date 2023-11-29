9d36dd6cfd9939d2f772156d9fafd42dfb324d21
Luis Metzger <luismetzger@me.com>
2023-11-28 19:48:45 -0700

    Use a bit of regex pattern matching to copy all string occurances. Make test pass using Gemfile and rspec setup.

--- simple_refactoring_exercise/spec/template_spec.rb
+++ simple_refactoring_exercise/spec/template_spec.rb
@@ -1,9 +1,9 @@
-require_relative 'template'
+require_relative '../template'
 
 describe Template do
   include Template
 
-  it "should substitute %CODE% and %ALTCODE% in the template" do
+  it 'should substitute %CODE% and %ALTCODE% in the template' do
     expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq 'Code is 5678901234; alt code is 56789-012'
   end
 
