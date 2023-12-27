a0e7689093395a6191bd556386c3761fd8a1f34f
Philip Leesha <philip.leesha@fitnesspass.pe>
2023-12-27 18:09:39 -0500

    refactor(simple_refactoring_exercise): Improve code readability and eliminate duplication in Template module
    - Extract common logic into private methods for code and altcode substitution
    - Use 'dup' instead of 'String.new' for string duplication
    - Rename methods for consistency and clarity
    - Introduce a new private method for substituting placeholders
    - Enhance flexibility to handle templates without '%ALTCODE%'
    - Add a new test case to cover templates without '%ALTCODE%'

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,26 +1,25 @@
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    template = source_template.dup
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = insert_code(template, req_id)
+    template = insert_altcode(template, req_id)
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    template
+  end
+
+  private
+
+  def insert_code(template, req_id)
+    update_placeholder(template, '%CODE%', req_id)
+  end
+
+  def insert_altcode(template, req_id)
+    altcode = req_id[0..4] + '-' + req_id[5..7]
+    update_placeholder(template, '%ALTCODE%', altcode)
+  end
+
+  def update_placeholder(template, placeholder, value)
+    template.gsub(placeholder, value)
   end
 end
