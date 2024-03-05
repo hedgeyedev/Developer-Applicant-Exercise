24de4ebb0dd67e8f2203ac9422ea4a1dbd13cce1
Charles Ren <cren04@syr.edu>
2024-03-04 19:03:08 -0800

    updated refactoring explanation

--- simple_refactoring_exercise/template.rb
+++ simple_refactoring_exercise/template.rb
@@ -1,7 +1,5 @@
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
-
     # Substitute for %CODE%
     code_sub = source_template.gsub('%CODE%', req_id)
 
@@ -9,7 +7,20 @@ module Template
     alt_sub = req_id.insert(5,'-')[0...-2]
     code_sub.gsub('%ALTCODE%', alt_sub)
 
-  
+    # Thought Process: 
+    # Line 4: We use .gsub to replace all displays of %CODE% in source_template with the provided req_id.
+    # Line 7: Inserts a hyphen into req_id string at the 5th index. The [0..-2] array range doesn't include
+    # the last two characters.
+    # Line 8: Similar to line 4, we want to remove all occurances of %ALTCODE% and substitute it with our
+    # altered alt_sub. Ruby returns the last line of a block so the altered code_sub variable with the 
+    # substitute for %CODE% & %ALTCODE& will be returned. 
+
+    # The provided code below is verbose and involved several instances of new variable creations and 
+    # multiple unnecessary concatenations. A simpler way is to replace all instances of %CODE% and the
+    # altered code of %ALTCODE%
+
+
+
     # # Substitute for %CODE%
     # template_split_begin = template.index("%CODE%")
     # template_split_end = template_split_begin + 6
